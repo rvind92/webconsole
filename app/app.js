3ff7bf4daae0936860321cd0331a7e48359bc8f4
Aravind Krishna <rvind92@gmail.com>
2016-08-30 11:31:07 +0800

    Created all the routes for the app

---
+++ app/app.js
@@ -0,0 +1,38 @@
+(function() {
+    
+    var app = angular.module('tanandApp', ['ngRoute']);
+    
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'LoginController',
+                templateUrl: 'app/views/login.html'
+            })
+            .when('/site', {
+                controller: 'SiteController',
+                templateUrl: 'app/views/site.html'
+            })
+            .when('/building', { 
+                controller: "BuildingController", 
+                templateUrl: "app/views/building.html"
+            })
+            .when('/floorplan', {
+                controller: "FloorplanController",
+                templateUrl: "app/views/floorplan.html"
+            })
+            .when('/sensor', {
+                controller: "SensorController",
+                templateUrl: "app/views/sensor.html"
+            })
+            .when('/edit_delete', {
+                controller: "EditDeleteController",
+                templateUrl: "app/views/edit_delete.html"
+            })
+            .when('/register', {
+                controller: "RegisterController",
+                templateUrl: "app/views/register.html"
+            })
+            .otherwise( { redirectTo: '/' } );
+    });
+    
+}());
